2bd29bd98513e85a02d753db756d63a301945de1
Truong2004-lost <132141078+Truong2004-lost@users.noreply.github.com>
2023-11-19 19:54:46 +0700

    Delete lung tung directory

--- lung tung/Bài 3.py
+++
@@ -1,9 +0,0 @@
-tong = 0
-n = int(input("Moi nhap so nguyen duong n: "))
-while not n > 10:
-    n = int(input("Moi nhap lai so nguyen duong n: "))
-      
-for i in range(1,n+1):
-    tong+=i
-
-print("Tong cac so tu 1 ->",n,"la: ",tong)
