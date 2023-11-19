6fd3d56dadd12d9c2ffd0bfbc728fe56faf0ec5e
Truong2004-lost <132141078+Truong2004-lost@users.noreply.github.com>
2023-11-19 19:53:59 +0700

    Delete .vscode directory

--- .vscode/scripts.js
+++
@@ -1,8 +0,0 @@
-// Your JavaScript code goes here
-$(document).ready(function () {
-    $("#loadDataBtn").click(function () {
-        // Simulate loading data
-        var data = "<p>Data loaded successfully!</p>";
-        $("#dataContainer").html(data);
-    });
-});
